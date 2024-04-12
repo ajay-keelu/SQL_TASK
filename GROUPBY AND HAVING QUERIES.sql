09ba5d36b303dd096cd3a81d33ebecc5f0a2e8b2
ajay-keelu <deepakdeva000@gmail.com>
2024-04-12 11:02:53 +0530

    Added Update Queries

--- GROUPBY AND HAVING QUERIES.sql
+++ GROUPBY AND HAVING QUERIES.sql
@@ -9,3 +9,12 @@ SELECT AVG(salary) AS Average FROM task_6_ajay.dbo.Employees WHERE title <> 'Fre
 
 --  What is the average age of employees of each unique title
 SELECT title,AVG(age) AS Average  FROM task_6_ajay.dbo.Employee GROUP BY title;
+
+-- In the age range of 25 to 40 get the number of employees under each unique title.
+SELECT title,COUNT(*) AS 'Number of employees'  FROM task_6_ajay.dbo.Employee WHERE age BETWEEN 25 AND 40 GROUP BY title ;
+
+-- Show the average salary of each unique title of employees only if the average salary is not less than 25000
+SELECT title,AVG(salary) AS Average  FROM task_6_ajay.dbo.Employee GROUP BY title HAVING AVG(salary) > 25000;
+
+-- Show the sum of ages of each unique title of employee only if the sum of age is greater than 30
+SELECT title,SUM(age) FROM task_6_ajay.dbo.Employee GROUP BY title HAVING SUM(age) > 30;
